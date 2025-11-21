225abffbf424667bb2e74c064bec2f616af13bf7
roy001 <peijie.jin@mail.utoronto.ca>
2025-11-20 19:30:16 -0500

    Finish checkstyle.

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ * Each play has a name and a type which determines pricing rules.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
